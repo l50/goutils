bcac8c4b354ee24a925f0c11388dad76703002cc
Jayson Grace <jayson.e.grace@gmail.com>
2019-05-13 18:38:10 -0700

    Add FileExists and unit test

--- fileutils.go
+++ fileutils.go
@@ -4,6 +4,14 @@ import (
 	"os"
 )
 
+func FileExists(fileLoc string) bool {
+	if _, err := os.Stat(fileLoc); !os.IsNotExist(err) {
+		return true
+	} else {
+		return false
+	}
+}
+
 func GetHomeDir() (string, error) {
 	out, err := os.UserHomeDir()
 
--- fileutils_test.go
+++ fileutils_test.go
@@ -1,8 +1,26 @@
 package utils
 
 import (
+	"path/filepath"
+	"runtime"
 	"testing"
 )
+
+func TestFileExists(t *testing.T) {
+	if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
+		exists := FileExists(filepath.FromSlash("/etc/passwd"))
+		if !exists {
+			t.Fatal("Unable to locate /etc/passwd, FileExists() failed.")
+		}
+	} else if runtime.GOOS == "windows" {
+		exists := FileExists(filepath.FromSlash("C:/WINDOWS/system32/win.ini"))
+		if !exists {
+			t.Fatal("Unable to locate C:/WINDOWS/system32/win.ini, FileExists() failed.")
+		}
+	} else {
+		t.Fatal("Unsupported OS detected")
+	}
+}
 
 func TestGetHomeDir(t *testing.T) {
 	_, err := GetHomeDir()
