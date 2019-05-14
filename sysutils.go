154c6ea8f081a7ecd444ace1d10c0d6192fa2f21
Jayson Grace <jayson.e.grace@gmail.com>
2019-05-14 14:39:52 -0700

    Add Cp and unit test

--- sysutils.go
+++ sysutils.go
@@ -1,8 +1,27 @@
 package utils
 
 import (
+	"io/ioutil"
+	"log"
 	"os/exec"
 )
+
+func Cp(src string, dst string) bool {
+	input, err := ioutil.ReadFile(src)
+	if err != nil {
+		log.Printf("Error reading %s:\n", src)
+		log.Println(err)
+		return false
+	}
+
+	err = ioutil.WriteFile(dst, input, 0644)
+	if err != nil {
+		log.Printf("Error creating %s:\n", dst)
+		log.Println(err)
+		return false
+	}
+	return true
+}
 
 // RunCommand runs a specified system command
 func RunCommand(cmd string, args ...string) (string, error) {
--- sysutils_test.go
+++ sysutils_test.go
@@ -1,10 +1,29 @@
 package utils
 
 import (
+	"os"
 	"runtime"
 	"strings"
 	"testing"
 )
+
+func TestCp(t *testing.T) {
+	file := "test.txt"
+	copyLoc := "testing.txt"
+	created := CreateEmptyFile(file)
+	if created {
+		copied := Cp(file, copyLoc)
+		if copied {
+			if FileExists(copyLoc) {
+				os.Remove(file)
+				os.Remove(copyLoc)
+			}
+		}
+	} else {
+		t.Fatal("File creation failed, check CreateEmptyFile() in fileutils.go for issues")
+	}
+
+}
 
 func TestRunCommand(t *testing.T) {
 	if runtime.GOOS == "linux" {
