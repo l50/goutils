aaeebe18308611430d8ee507189bdcf5f2fc595d
Jayson Grace <jayson.e.grace@gmail.com>
2022-05-15 16:16:35 -0600

    Overhaul CloneRepo() and associated test

--- gitutils.go
+++ gitutils.go
@@ -1,25 +1,85 @@
 package utils
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
 	"github.com/fatih/color"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
+	"github.com/go-git/go-git/v5/storage/memory"
 )
 
-// CloneRepo clones the repo specified at src to the path
-// specified with dst
-func CloneRepo(src string, dst string) bool {
-	_, err := git.PlainClone(dst, false, &git.CloneOptions{
-		URL:      src,
-		Progress: os.Stdout,
-	})
+var (
+	errMsg string
+)
 
+// SSHKeyInfo is used to hold the name of an SSH Key
+// file and a password for decryption.
+type SSHKeyInfo struct {
+	Name string
+	PW   string
+}
+
+// GetSSHPubKey returns the public SSH key for the input
+// `keyName` and uses the input `password` (if provided)
+// to decrypt the associated private key.
+// Resource: https://medium.com/@clm160/tag-example-with-go-git-library-4377a84bbf17
+func GetSSHPubKey(keyName string, password string) (*ssh.PublicKeys, error) {
+	var publicKey *ssh.PublicKeys
+
+	sshPath := os.Getenv("HOME") + "/.ssh/" + keyName
+	publicKey, err := ssh.NewPublicKeysFromFile("git", sshPath, password)
 	if err != nil {
-		fmt.Print(color.RedString("failed to clone %s to %s: %v", src, dst, err))
-		return false
+		return nil, err
 	}
 
-	return true
+	return publicKey, nil
+}
+
+// CloneRepo clones the repo specified with the input `url` to
+// `dstPathPath`.
+//
+// To clone a repo using an SSH key, provide
+// the name of the key file for `sshKey.Name`.
+func CloneRepo(url string, dstPath string, sshKey SSHKeyInfo) (
+	*git.Repository, error) {
+	var err error
+	var repo *git.Repository
+	var cloneOptions *git.CloneOptions
+
+	if sshKey.Name != "" {
+		pubKey, err := GetSSHPubKey(sshKey.Name, sshKey.PW)
+		if err != nil {
+			errMsg = fmt.Sprint(color.RedString(
+				"%s failed to get %s SSH key: %v", sshKey.Name, err))
+			return nil, errors.New(errMsg)
+		}
+		cloneOptions = &git.CloneOptions{
+			URL:      url,
+			Progress: os.Stdout,
+			Auth:     pubKey,
+		}
+	} else {
+		cloneOptions = &git.CloneOptions{
+			URL:      url,
+			Progress: os.Stdout,
+		}
+	}
+
+	repo, err = git.Clone(memory.NewStorage(), nil, cloneOptions)
+
+	if err != nil {
+		if err == git.ErrRepositoryAlreadyExists {
+			errMsg = fmt.Sprint(color.RedString(
+				"%s was already cloned to %s", url, dstPath))
+			return nil, errors.New(errMsg)
+		}
+		errMsg = fmt.Sprint(color.RedString(
+			"failed to clone %s to %s: %v", url, dstPath, err))
+		return nil, errors.New(errMsg)
+	}
+
+	return repo, nil
 }
--- gitutils_test.go
+++ gitutils_test.go
@@ -1,15 +1,41 @@
 package utils
 
-// func TestCloneRepo(t *testing.T) {
-// 	targetDir := "/tmp"
-// 	repo := "https://github.com/l50/helloworld.git"
-// 	currentTime := time.Now()
-// 	cloneLoc := filepath.Join(targetDir, fmt.Sprintf("helloworld-%s", currentTime.Format("2006-01-02-15-04-05")))
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
 
-// 	cloned := CloneRepo(repo, cloneLoc)
-// 	if !cloned {
-// 		t.Fatalf("failed to clone %s", repo)
-// 	}
+func TestCloneRepo(t *testing.T) {
+	ogDir := Gwd()
+	targetDir := "/tmp"
+	repo := "https://github.com/l50/helloworld.git"
+	currentTime := time.Now()
+	cloneLoc := filepath.Join(
+		targetDir, fmt.Sprintf(
+			"helloworld-%s", currentTime.Format("2006-01-02-15-04-05"),
+		),
+	)
 
-// 	defer os.RemoveAll(cloneLoc)
-// }
+	if err := Cd(targetDir); err != nil {
+		if err != nil {
+			t.Fatalf("failed to cd to %s: %v - Cd() failed",
+				targetDir, err)
+		}
+	}
+
+	if _, err := CloneRepo(repo, cloneLoc, SSHKeyInfo{}); err != nil {
+		t.Fatalf("failed to clone %s: %v", repo, err)
+	}
+
+	if err := Cd(ogDir); err != nil {
+		if err != nil {
+			t.Fatalf("failed to cd to %s: %v - Cd() failed",
+				ogDir, err)
+		}
+	}
+
+	os.RemoveAll(cloneLoc)
+}
