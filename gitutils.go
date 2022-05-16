8e5da164ba7d0932274f40a5569f057d3b2f1d9f
Jayson Grace <jayson.e.grace@gmail.com>
2022-05-15 23:48:29 -0600

    Numerous gitutils additions and improvements
    - Fixed minor bugs
    - Made CloneRepo() work for multiple forms of auth
    - Overhauled and DRY'ed up tests

--- gitutils.go
+++ gitutils.go
@@ -4,16 +4,28 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/fatih/color"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/transport"
 	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
-	"github.com/go-git/go-git/v5/storage/memory"
+	"github.com/magefile/mage/sh"
 )
 
 var (
 	errMsg string
+	tags   []string
 )
+
+// GitConfigUserInfo holds a username and
+// email to use for user.name and user.email.
+type GitConfigUserInfo struct {
+	User  string
+	Email string
+}
 
 // SSHKeyInfo is used to hold the name of an SSH Key
 // file and a password for decryption.
@@ -25,7 +37,6 @@ type SSHKeyInfo struct {
 // GetSSHPubKey returns the public SSH key for the input
 // `keyName` and uses the input `password` (if provided)
 // to decrypt the associated private key.
-// Resource: https://medium.com/@clm160/tag-example-with-go-git-library-4377a84bbf17
 func GetSSHPubKey(keyName string, password string) (*ssh.PublicKeys, error) {
 	var publicKey *ssh.PublicKeys
 
@@ -39,27 +50,21 @@ func GetSSHPubKey(keyName string, password string) (*ssh.PublicKeys, error) {
 }
 
 // CloneRepo clones the repo specified with the input `url` to
-// `dstPathPath`.
+// `clonePath`.
 //
 // To clone a repo using an SSH key, provide
 // the name of the key file for `sshKey.Name`.
-func CloneRepo(url string, dstPath string, sshKey SSHKeyInfo) (
+func CloneRepo(url string, clonePath string, auth transport.AuthMethod) (
 	*git.Repository, error) {
 	var err error
 	var repo *git.Repository
 	var cloneOptions *git.CloneOptions
 
-	if sshKey.Name != "" {
-		pubKey, err := GetSSHPubKey(sshKey.Name, sshKey.PW)
-		if err != nil {
-			errMsg = fmt.Sprint(color.RedString(
-				"%s failed to get %s SSH key: %v", sshKey.Name, err))
-			return nil, errors.New(errMsg)
-		}
+	if auth != nil {
 		cloneOptions = &git.CloneOptions{
 			URL:      url,
 			Progress: os.Stdout,
-			Auth:     pubKey,
+			Auth:     auth,
 		}
 	} else {
 		cloneOptions = &git.CloneOptions{
@@ -68,18 +73,182 @@ func CloneRepo(url string, dstPath string, sshKey SSHKeyInfo) (
 		}
 	}
 
-	repo, err = git.Clone(memory.NewStorage(), nil, cloneOptions)
-
+	repo, err = git.PlainClone(clonePath, false, cloneOptions)
 	if err != nil {
 		if err == git.ErrRepositoryAlreadyExists {
 			errMsg = fmt.Sprint(color.RedString(
-				"%s was already cloned to %s", url, dstPath))
+				"%s was already cloned to %s", url, clonePath))
 			return nil, errors.New(errMsg)
 		}
 		errMsg = fmt.Sprint(color.RedString(
-			"failed to clone %s to %s: %v", url, dstPath, err))
+			"failed to clone %s to %s: %v", url, clonePath, err))
 		return nil, errors.New(errMsg)
 	}
 
 	return repo, nil
+}
+
+// GetTags returns the tags for an input `repo`.
+func GetTags(repo *git.Repository) ([]string, error) {
+	tagObjects, err := repo.TagObjects()
+	if err != nil {
+		errMsg = fmt.Sprint(color.RedString(
+			"failed to retrieve repo tags: %v", err))
+		return tags, errors.New(errMsg)
+	}
+
+	err = tagObjects.ForEach(func(t *object.Tag) error {
+		tags = append(tags, t.Name)
+		return nil
+	})
+
+	if err != nil {
+		errMsg = fmt.Sprint(color.RedString(
+			"failed to retrieve repo tags: %v", err))
+		return tags, errors.New(errMsg)
+	}
+
+	return tags, err
+}
+
+func tagExists(repo *git.Repository, tag string) (bool, error) {
+	tags, err := GetTags(repo)
+	if err != nil {
+		return false, err
+	}
+
+	for _, t := range tags {
+		if t == tag {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+// GetGlobalUserCfg returns the username and email from
+// the global git user settings.
+func GetGlobalUserCfg() (GitConfigUserInfo, error) {
+	userInfo := GitConfigUserInfo{}
+	var err error
+
+	userInfo.User, err = sh.Output("git", "config", "user.name")
+	if err != nil {
+		return userInfo, fmt.Errorf(color.RedString(
+			"failed to retrieve global git username: %v", err))
+	}
+
+	userInfo.Email, err = sh.Output("git", "config", "user.email")
+	if err != nil {
+		return userInfo, fmt.Errorf(color.RedString(
+			"failed to retrieve global git email: %v", err))
+	}
+
+	return userInfo, nil
+}
+
+// CreateTag is used to create an input `tag` in the
+// specified `repo` if it doesn't already exist.
+// Resource: https://github.com/go-git/go-git/blob/bf3471db54b0255ab5b159005069f37528a151b7/_examples/tag-create-push/main.go
+func CreateTag(repo *git.Repository, tag string) error {
+	exists, err := tagExists(repo, tag)
+	if err != nil {
+		return fmt.Errorf(color.RedString(
+			"failed to retrieve repo tags: %v", err))
+	}
+
+	if exists {
+		return fmt.Errorf(color.RedString(
+			"error creating input tag %s: it already exists", tag))
+	}
+
+	cfg, err := GetGlobalUserCfg()
+	if err != nil {
+		return fmt.Errorf(color.RedString(
+			"failed get repo config: %v", err))
+	}
+
+	h, err := repo.Head()
+	if err != nil {
+		return fmt.Errorf(color.RedString(
+			"failed to get repo head: %v", err))
+	}
+
+	_, err = repo.CreateTag(tag, h.Hash(), &git.CreateTagOptions{
+		Tagger: &object.Signature{
+			Name:  cfg.User,
+			Email: cfg.Email,
+			When:  time.Now(),
+		},
+		Message: tag,
+	})
+
+	if err != nil {
+		return fmt.Errorf(color.RedString(
+			"error creating input tag %s: %v", tag, err))
+	}
+
+	return nil
+}
+
+// PushTag is used to push a tag to remote.
+func PushTag(repo *git.Repository, tag string, auth transport.AuthMethod) error {
+	pushOptions := &git.PushOptions{
+		RemoteName: "origin",
+		Progress:   os.Stdout,
+		RefSpecs: []config.RefSpec{config.RefSpec(
+			"refs/tags/*:refs/tags/*")},
+		Auth: auth,
+	}
+
+	err := repo.Push(pushOptions)
+
+	if err != nil {
+		if err == git.NoErrAlreadyUpToDate {
+			fmt.Print(color.YellowString(
+				"origin remote is up-to-date, no push was executed."))
+			return nil
+		}
+
+		return fmt.Errorf(color.RedString(
+			"error pushing %s tag to remote origin: %v", tag, err))
+	}
+
+	return nil
+}
+
+// DeleteTag deletes the local input `tag` from the
+// specified repo.
+func DeleteTag(repo *git.Repository, tag string) error {
+	if err := repo.DeleteTag(tag); err != nil {
+		return fmt.Errorf(color.RedString(
+			"error deleting local %s tag: %v", tag, err))
+	}
+
+	return nil
+}
+
+// DeletePushedTag deletes an input `tag` that has been pushed
+// to remote.
+func DeletePushedTag(repo *git.Repository, tag string, auth transport.AuthMethod) error {
+	err := repo.Push(&git.PushOptions{
+		RemoteName: "origin",
+		Progress:   os.Stdout,
+		RefSpecs: []config.RefSpec{config.RefSpec(
+			"refs/tags/" + tag)},
+		Auth: auth,
+	})
+
+	if err != nil {
+		if err == git.NoErrAlreadyUpToDate {
+			fmt.Print(color.YellowString(
+				"origin remote is up-to-date, no delete was executed."))
+			return nil
+		}
+
+		return fmt.Errorf(color.RedString(
+			"error deleting pushed tag %s: %v", tag, err))
+	}
+
+	return nil
 }
--- gitutils_test.go
+++ gitutils_test.go
@@ -2,40 +2,104 @@ package utils
 
 import (
 	"fmt"
-	"os"
+	"log"
 	"path/filepath"
 	"testing"
 	"time"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
 )
 
-func TestCloneRepo(t *testing.T) {
-	ogDir := Gwd()
-	targetDir := "/tmp"
-	repo := "https://github.com/l50/helloworld.git"
+var (
+	cloneDir  string
+	clonePath string
+	err       error
+	repo      *git.Repository
+	repoURL   string
+)
+
+func init() {
+	cloneDir = "/tmp"
+	repoURL = "https://github.com/l50/helloworld.git"
+	// Used to create a random directory name
 	currentTime := time.Now()
-	cloneLoc := filepath.Join(
-		targetDir, fmt.Sprintf(
+	clonePath = filepath.Join(
+		cloneDir, fmt.Sprintf(
 			"helloworld-%s", currentTime.Format("2006-01-02-15-04-05"),
 		),
 	)
 
-	if err := Cd(targetDir); err != nil {
-		if err != nil {
-			t.Fatalf("failed to cd to %s: %v - Cd() failed",
-				targetDir, err)
-		}
+	repo, err = CloneRepo(repoURL, clonePath, nil)
+	if err != nil {
+		log.Fatalf(
+			"failed to clone %s: %v - CloneRepo() failed",
+			repo,
+			err,
+		)
+	}
+}
+
+func TestGetTags(t *testing.T) {
+	if _, err := GetTags(repo); err != nil {
+		t.Fatalf("failed to get tags: %v - GetTags() failed", err)
+	}
+}
+
+func TestPushTag(t *testing.T) {
+	tag := "v6.6.6"
+
+	if err := CreateTag(repo, tag); err != nil {
+		t.Fatalf("failed to create %s tag: %v", tag, err)
 	}
 
-	if _, err := CloneRepo(repo, cloneLoc, SSHKeyInfo{}); err != nil {
-		t.Fatalf("failed to clone %s: %v", repo, err)
+	// personal access token example
+	token := "notrealtoken"
+	auth := &http.BasicAuth{
+		// this can be anything except for an empty string
+		Username: "abc123",
+		Password: token,
 	}
 
-	if err := Cd(ogDir); err != nil {
-		if err != nil {
-			t.Fatalf("failed to cd to %s: %v - Cd() failed",
-				ogDir, err)
-		}
+	if err := PushTag(repo, tag, auth); err == nil {
+		t.Fatal("pushing any tag should not be possible "+
+			"because no auth mechanism is configured - "+
+			"PushTag() failed",
+			err)
+	}
+}
+
+func TestGetGlobalUserCfg(t *testing.T) {
+	cfg, err := GetGlobalUserCfg()
+	if err != nil || cfg.User == "" {
+		t.Fatalf("failed get global git user config: %v", err)
+	}
+}
+
+func TestDeletePushedTag(t *testing.T) {
+	tag := "v7.7.7"
+	keyName := "github_rsa"
+
+	if err := CreateTag(repo, tag); err != nil {
+		t.Fatalf("failed to create %s tag: %v", tag, err)
 	}
 
-	os.RemoveAll(cloneLoc)
+	if err := DeleteTag(repo, tag); err != nil {
+		t.Fatalf("failed to delete %s tag: %v - DeleteTag() failed",
+			tag, err)
+	}
+
+	pubKey, err := GetSSHPubKey(keyName, "")
+	if err == nil {
+		t.Fatalf("expected to fail with %s as this key should "+
+			"always be encrypted at rest and we didn't provide a"+
+			"decryption key: %v", keyName, err)
+	}
+
+	if err := DeletePushedTag(repo, tag, pubKey); err == nil {
+		t.Fatal("deleting any tag should not be possible " +
+			"in this test. There are not sufficient permissions " +
+			"from the previous steps to do so - " +
+			"DeletePushedTag() failed")
+	}
 }
