a91a0a2f59e763c4aa77ec5001a4b8c3cc47ad36
Jayson Grace <jayson.e.grace@gmail.com>
2023-05-23 10:48:28 -0600

    Add net package and tests

---
+++ v2/net/net.go
@@ -0,0 +1,86 @@
+package net
+
+import (
+	"fmt"
+
+	"github.com/cavaliergopher/grab/v3"
+	"github.com/fatih/color"
+	externalip "github.com/glendc/go-external-ip"
+)
+
+// PublicIP uses several external services to get the public IP address of the system running it, using the
+// package github.com/GlenDC/go-external-ip. The function takes an IP protocol version (4 or 6) as input and
+// returns the public IP address as a string or an error.
+//
+// Parameters:
+//
+// protocol: A uint representing the IP protocol version (4 or 6).
+//
+// Returns:
+//
+// string: The public IP address of the system in string format.
+// error: An error if the function fails to retrieve the public IP address.
+//
+// Example:
+//
+// protocol := uint(4) // or 6 for IPv6
+// ip, err := PublicIP(protocol)
+//
+//	if err != nil {
+//	  log.Fatalf("failed to get public IP address: %v", err)
+//	}
+//
+// log.Printf("Public IP address: %s\n", ip)
+func PublicIP(protocol uint) (string, error) {
+
+	// Create the default consensus with the default config
+	// and no logger.
+	consensus := externalip.DefaultConsensus(nil, nil)
+
+	if err := consensus.UseIPProtocol(protocol); err != nil {
+		return "", fmt.Errorf(color.RedString("failed to get public IP address: %v", err))
+	}
+
+	// Retrieve the external IP address.
+	ip, err := consensus.ExternalIP()
+	if err != nil {
+		return "", fmt.Errorf(color.RedString("failed to get public IP address: %v", err))
+	}
+
+	// Return the IP address in string format.
+	return ip.String(), nil
+}
+
+// DownloadFile downloads a file from the provided URL and saves it to the specified location on the local
+// filesystem. The function takes the source URL and the destination path as inputs and returns the path
+// where the file was saved or an error.
+//
+// Parameters:
+//
+// url: A string representing the URL of the file to be downloaded.
+// dest: A string representing the destination path where the file should be saved on the local filesystem.
+//
+// Returns:
+//
+// string: The path where the downloaded file was saved.
+// error: An error if the function fails to download the file.
+//
+// Example:
+//
+// url := "http://example.com/path/to/file"
+// dest := "/path/to/save/location"
+// file, err := DownloadFile(url, dest)
+//
+//	if err != nil {
+//	  log.Fatalf("failed to download file: %v", err)
+//	}
+//
+// log.Printf("File downloaded to: %s\n", file)
+func DownloadFile(url string, dest string) (string, error) {
+	resp, err := grab.Get(dest, url)
+	if err != nil {
+		return resp.Filename, fmt.Errorf(color.RedString("failed to download %s to %s: %v", url, dest, err))
+	}
+
+	return resp.Filename, nil
+}
---
+++ v2/net/net_test.go
@@ -0,0 +1,66 @@
+package net_test
+
+import (
+	"testing"
+
+	"github.com/l50/goutils/v2/net"
+)
+
+func TestPublicIP(t *testing.T) {
+	testCases := []struct {
+		name     string
+		protocol uint
+		wantErr  bool
+	}{
+		{
+			name:     "IPv4 public IP",
+			protocol: 4,
+			wantErr:  false,
+		},
+		{
+			name:     "IPv6 public IP",
+			protocol: 6,
+			wantErr:  true, // Considering that a lot of networks aren't using IPv6
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := net.PublicIP(tc.protocol)
+			if (err != nil) != tc.wantErr {
+				t.Errorf("PublicIP() error = %v, wantErr %v", err, tc.wantErr)
+			}
+		})
+	}
+}
+
+func TestDownloadFile(t *testing.T) {
+	testCases := []struct {
+		name    string
+		url     string
+		dest    string
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "Successfully download file",
+			url:     "https://raw.githubusercontent.com/l50/helloworld/master/hello.go",
+			dest:    "/tmp/hello.go",
+			want:    "/tmp/hello.go",
+			wantErr: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := net.DownloadFile(tc.url, tc.dest)
+			if (err != nil) != tc.wantErr {
+				t.Errorf("DownloadFile() error = %v, wantErr %v", err, tc.wantErr)
+				return
+			}
+			if got != tc.want {
+				t.Errorf("DownloadFile() = %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
