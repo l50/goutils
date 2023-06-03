ef6704e1e368c842418b658569fd2e221d4097b3
Jayson Grace <jayson.e.grace@gmail.com>
2023-06-03 12:45:40 -0600

    Update go-vet to account for all hidden directories

--- .hooks/go-vet.sh
+++ .hooks/go-vet.sh
@@ -5,7 +5,7 @@ pkgs=$(go list ./...)
 
 for pkg in $pkgs; do
     dir="$(basename "$pkg")/"
-    if [[ "${dir}" != ".hooks/" ]] && [[ "${dir}" != "magefiles/" ]]; then
+    if [[ "${dir}" != .*/ ]] && [[ "${dir}" != "magefiles/" ]]; then
         go vet "${pkg}/${dir}"
     fi
 done
